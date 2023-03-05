120865daf23c97753caf324f93164dbf84ba890a
Alexander <a.schmidt.2002@gmx.de>
2023-03-05 22:44:43 +0100

    fixed?

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require("express")
-const https = require('https');
-const fs = require("mz/fs");
 // require('bootstrap')
+const port = 3000
+
 const app = express(); // add this line
-
-const {key, cert} = async () => {
-	const certdir = (await fs.readdir("/etc/letsencrypt/live"))[0];
-
-	return {
-		key: await fs.readFile(`/etc/letsencrypt/live/${certdir}/privkey.pem`),
-		cert: await fs.readFile(`/etc/letsencrypt/live/${certdir}/fullchain.pem`)
-	}
-};
 
 app.get('/', (req, res) => {
     return res.send("Hello World! Meine app läuft auf dem Server")
-})
-    
-const httpsServer = https.createServer({key, cert}, app).listen(4433)
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
